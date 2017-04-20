d462eea13e62808e67715c8d3b0e7baa8ad13936
Jeroen Böttcher <Jeroen Bttcher>
2017-04-20 12:20:44 +0200

    server.js aangepast

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var http = require('http');
 
-function onRequest(request, response){
+http.createServer(function(request, response){
 	console.log('er was een request');
 	response.writeHead(200,{'Content-Type':'text/html'});
-	response.write('<h1>hello world</h1>');
-	response.end();
-}
-
-http.createServer(onRequest).listen(3000);
+	var json = JSON.stringify({
+		tekst:"Dit is JSON"
+	});
+	response.end(json);
+}).listen(process.env.PORT || 3000);
 
 console.log('de server luistert op port 3000');
