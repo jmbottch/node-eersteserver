c6cb8479bfdbf42a8c93cf168944f066598b46da
Jeroen Böttcher <Jeroen Bttcher>
2017-04-20 11:41:58 +0200

    Uitroepteken verwijderd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 function onRequest(request, response){
 	console.log('er was een request');
 	response.writeHead(200,{'Content-Type':'text/html'});
-	response.write('<h1>hello world!</h1>');
+	response.write('<h1>hello world</h1>');
 	response.end();
 }
 
